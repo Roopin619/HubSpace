879d20d1ff8af8f216e4d0a17033dbff73b84d49
Roopin Bhadri <bhadriroopin748@gmail.com>
2020-05-28 07:49:28 +0530

    Updates

---
+++ precache-manifest.fb050b3b0f6d6a3784c4e417049b5033.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f51fdde07abd242017b34f4a859d0f9",
+    "url": "/HubSpace/index.html"
+  },
+  {
+    "revision": "54a51b3384072b796b4a",
+    "url": "/HubSpace/static/css/main.c54f1878.chunk.css"
+  },
+  {
+    "revision": "a8bc2a022226dacd30ed",
+    "url": "/HubSpace/static/js/2.16f0c8e5.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/HubSpace/static/js/2.16f0c8e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a51b3384072b796b4a",
+    "url": "/HubSpace/static/js/main.3603eead.chunk.js"
+  },
+  {
+    "revision": "2ad48ddfae608b6428ca",
+    "url": "/HubSpace/static/js/runtime-main.5b21e2c5.js"
+  },
+  {
+    "revision": "5d9b5cae4b21ad5b14864ecfc3f8304c",
+    "url": "/HubSpace/static/media/Atlassian-logo.5d9b5cae.png"
+  },
+  {
+    "revision": "3feec6f6420f29c11c45e5f09038cd5e",
+    "url": "/HubSpace/static/media/Classpass-logo.3feec6f6.png"
+  },
+  {
+    "revision": "6ffe79306c019327603822b53a88a20f",
+    "url": "/HubSpace/static/media/DoorDash-logo.6ffe7930.png"
+  },
+  {
+    "revision": "32829d6330dcef3745efc11aaef90802",
+    "url": "/HubSpace/static/media/G2-Crowd-logo.32829d63.png"
+  },
+  {
+    "revision": "7c7e0bf4d3ad0301a688c94a225a1378",
+    "url": "/HubSpace/static/media/Subaru-logo.7c7e0bf4.png"
+  },
+  {
+    "revision": "d52dcb1dc16bc06ad98aaede1d84249f",
+    "url": "/HubSpace/static/media/SurveyMonkey-logo.d52dcb1d.png"
+  },
+  {
+    "revision": "27bd2166890a2d646c9c26cdea5f439e",
+    "url": "/HubSpace/static/media/Vmware-logo.27bd2166.png"
+  },
+  {
+    "revision": "f13792e189631462ad7ca73cf4cfc8e3",
+    "url": "/HubSpace/static/media/Wistia-logo.f13792e1.png"
+  },
+  {
+    "revision": "341f34ba8ba86f1b7bd57c4dfe3857cd",
+    "url": "/HubSpace/static/media/chat.341f34ba.png"
+  },
+  {
+    "revision": "bbf68fa5896f9709cc4d1c6b7298dde1",
+    "url": "/HubSpace/static/media/hero.bbf68fa5.svg"
+  },
+  {
+    "revision": "62bff120184128143aff0455c4236853",
+    "url": "/HubSpace/static/media/profile-pic.62bff120.png"
+  }
+]);
